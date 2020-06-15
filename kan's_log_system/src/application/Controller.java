14a952986e258e8da927bba70be995cdc1b608b4
kantu310 <66015368+kantu310@users.noreply.github.com>
2020-06-15 23:17:01 +0900

    メインメニュー更新

--- kan's_log_system/src/application/Controller.java
+++ kan's_log_system/src/application/Controller.java
@@ -7,16 +7,54 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import parts.UserParts;
+
 
 public class Controller extends Main{
 	@FXML
 	public TextField login_user_id;
 	public TextField login_password;
 	public Label login_error_message;
+	Stage stage = new Stage();
+	Scene scene;
+
+    @FXML
+    private Pane pane_top;
+
+    @FXML
+    private Pane pane_loan;
+
+    @FXML
+    private Pane pane_movie;
+
+    @FXML
+    private Pane pane_book;
+
+    @FXML
+    private Pane pane_account;
+
+    @FXML
+    private Button btn_top;
+
+    @FXML
+    private Button btn_book;
+
+    @FXML
+    private Button btn_loan;
+
+    @FXML
+    private Button btn_movie;
+
+    @FXML
+    private Button btn_account;
+
+
 
 	public void onButtonLogin(Event event) throws Exception {
 
@@ -31,8 +69,8 @@ public class Controller extends Main{
 			System.out.println("ログイン成功だよ！");
 			((Node) event.getSource()).getScene().getWindow().hide();
 			Parent parent = FXMLLoader.load(getClass().getResource("Mainmenu.fxml"));
-			Stage stage = new Stage();
-			Scene scene = new Scene(parent,1650,750);
+
+			scene = new Scene(parent);
 			stage.setScene(scene);
 			stage.setTitle("Kan's Log System");
 			stage.show();
@@ -43,19 +81,19 @@ public class Controller extends Main{
 		}
 
 	}
-
+    @FXML
 	public void onButtonLoan(Event event) throws Exception {
-
-
+		//Parent parent = FXMLLoader.load(getClass().getResource("Loan.fxml"));
+		//Stage stage = new Stage();
+		//Scene scene = new Scene(parent,615,750);
+		//stage.setScene(scene);
+		//stage.setTitle("Kan's Log System");
+		//stage.show();
 		//((Node) event.getSource()).getScene().getWindow().hide();
-		Parent parent = FXMLLoader.load(getClass().getResource("Loan.fxml"));
-		Stage stage = new Stage();
-		Scene scene = new Scene(parent,1650,750);
-		stage.setScene(scene);
-		stage.setTitle("Kan's Log System");
-		stage.show();
-
-
+		AnchorPane pane = FXMLLoader.load(getClass().getResource("Loan.fxml"));
+		pane_top.getChildren().setAll(pane);
 	}
+
+
 }
 
