77fbcd9c35aabecf272a45ec2868926195b59995
kantu310 <66015368+kantu310@users.noreply.github.com>
2020-07-11 00:04:01 +0900

    年ごとの表示実装

--- kan's_log_system/src/application/MovieController.java
+++ kan's_log_system/src/application/MovieController.java
@@ -5,6 +5,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import data.ConstantData;
 import javafx.collections.FXCollections;
@@ -14,6 +17,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Text;
@@ -23,59 +27,95 @@ import table.Movie;
 public class MovieController extends MainmenuController {
 
 	public static ObservableList<Movie> movieList = FXCollections.observableArrayList();
+	public static List<String> movieYearList = new ArrayList<>();
+	public static int yearCnt = 0;
+	public static int yearMaxCnt;
+	SimpleDateFormat smfyear = new SimpleDateFormat("yyyy");
 
 
-    @FXML
-    private AnchorPane moviepane;
+	@FXML
+	private AnchorPane moviepane;
 
-    @FXML
-    private ScrollPane movieScrPane;
+	@FXML
+	private ScrollPane movieScrPane;
 
-    @FXML
-    private FlowPane movieFlowPane;
+	@FXML
+	private FlowPane movieFlowPane;
 
-    @FXML
-    private Text movie_year;
+	@FXML
+	private Text movie_year;
+
+	@FXML
+	private ImageView btnMovieYearBk;
+
+	@FXML
+	private ImageView btnMovieYearFr;
 
 
-    @FXML
+	@FXML
 	private void initialize() throws SQLException {
-    	movieImageDisplay();
-    }
+		movieList = MovieParts.getMovie();//movieテーブルから情報を取得
+		//鑑賞日付から年を抽出し、リストを作成
+		for (Movie movie : movieList) {
+			String year = smfyear.format(movie.movie_date);
+			//重複を除いて年をリストに追加
+			if(!movieYearList.contains(year)){
+				movieYearList.add(year);
+			}
+		}
+		yearMaxCnt = movieYearList.size()-1;//鑑賞年の最大インデックスを格納
 
-    @FXML
-    public void movieImageDisplay() throws SQLException {
+		movie_year.setText(String.valueOf(movieYearList.get(yearCnt)));
 
-    	byte[] r = null;
-    	movieList = MovieParts.getMovie();
+		//ボタンの初期化
+		if(yearCnt != 0) {
+			btnMovieYearFr.setVisible(true);
+		}
+		if(yearCnt == yearMaxCnt) {
+			btnMovieYearBk.setVisible(false);
+		}
+		movieImageDisplay(yearCnt);
+	}
 
-    	for (Movie movie : movieList) {
-    		InputStream is = movie.movie_image.getBinaryStream();
-    		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    		byte[] bs = new byte [1024];
-    		int size = 0;
-    		try {
-    			while( ( size = is.read( bs ) ) != -1 ){
-    				baos.write( bs, 0, size );
-    			}
-    			r = baos.toByteArray();  //. byte[] 型に変換してデータを取得
-    			Image img = new Image(new ByteArrayInputStream(r));
-    			ImageView imgv = new ImageView(img);
-    			imgv.setFitHeight(280);
-    			imgv.setFitWidth(180);
-    			movieFlowPane.getChildren().addAll(imgv);
-    			imgv.setOnMouseClicked(event -> movieDetailDisplay(movie.movie_id));
-    		} catch (IOException e) {
-    			e.printStackTrace();
-    			r = null;
-    		}
-    	}
-    }
+	@FXML
+	public void movieImageDisplay(int a) throws SQLException {
+
+		byte[] r = null;
 
 
-    @FXML
-    public void movieDetailDisplay(int a) {
-    	ConstantData.setMovie_id(a);
+		//映画イメージを表示
+		for (Movie movie : movieList) {
+
+			String year = smfyear.format(movie.movie_date);
+
+			if(year.equals(String.valueOf(movieYearList.get(a)))) {
+				InputStream is = movie.movie_image.getBinaryStream();
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				byte[] bs = new byte [1024];
+				int size = 0;
+				try {
+					while( ( size = is.read( bs ) ) != -1 ){
+						baos.write( bs, 0, size );
+					}
+					r = baos.toByteArray();  //. byte[] 型に変換してデータを取得
+					Image img = new Image(new ByteArrayInputStream(r));
+					ImageView imgv = new ImageView(img);
+					imgv.setFitHeight(280);
+					imgv.setFitWidth(180);
+					movieFlowPane.getChildren().addAll(imgv);
+					imgv.setOnMouseClicked(event -> movieDetailDisplay(movie.movie_id));
+				} catch (IOException e) {
+					e.printStackTrace();
+					r = null;
+				}
+			}
+		}
+	}
+
+
+	@FXML
+	public void movieDetailDisplay(int a) {
+		ConstantData.setMovie_id(a);
 		AnchorPane pane;
 		try {
 			pane = FXMLLoader.load(getClass().getResource("Moviedetail.fxml"));
@@ -85,7 +125,45 @@ public class MovieController extends MainmenuController {
 			e.printStackTrace();
 		}
 
-    }
+	}
+
+	@FXML
+	void onBtnMovieYearBk(MouseEvent event) {
+		btnMovieYearFr.setVisible(true);
+		yearCnt = yearCnt +1;
+			try {
+				movieFlowPane.getChildren().clear();
+				movie_year.setText(String.valueOf(movieYearList.get(yearCnt)));
+				movieImageDisplay(yearCnt);
+			} catch (SQLException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+
+			if(yearCnt==yearMaxCnt) {
+				btnMovieYearBk.setVisible(false);
+			}
+
+	}
+
+	@FXML
+	void onBtnMovieYearFr(MouseEvent event) {
+		btnMovieYearBk.setVisible(true);
+		yearCnt = yearCnt -1;
+			try {
+				movieFlowPane.getChildren().clear();
+				movie_year.setText(String.valueOf(movieYearList.get(yearCnt)));
+				movieImageDisplay(yearCnt);
+			} catch (SQLException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+
+			if(yearCnt==0) {
+				btnMovieYearFr.setVisible(false);
+			}
+
+	}
 
 
 }
